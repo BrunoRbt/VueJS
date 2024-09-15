8e64757bde523ee523e7fc597f33a7dd79d55a22
Bruno <145143785+BrunoRbt@users.noreply.github.com>
2024-09-14 23:26:50 -0300

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Calculadora Aritmética</h1>
+    <div>
+      <input type="number" v-model.number="numero1" />
+      <select v-model="operacao">
+        <option value="+">Soma</option>
+        <option value="-">Subtração</option>
+        <option value="*">Multiplicação</option>
+        <option value="/">Divisão</option>
+      </select>
+      <input type="number" v-model.number="numero2" />
+    </div>
+    <h2>Resultado: {{ resultado }}</h2>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: '+',
+    };
+  },
+  computed: {
+    resultado() {
+      switch (this.operacao) {
+        case '+':
+          return this.numero1 + this.numero2;
+        case '-':
+          return this.numero1 - this.numero2;
+        case '*':
+          return this.numero1 * this.numero2;
+        case '/':
+          return this.numero2 !== 0 ? this.numero1 / this.numero2 : 'Erro: Divisão por zero';
+        default:
+          return 0;
+      }
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+input {
+  margin: 10px;
 }
 </style>
